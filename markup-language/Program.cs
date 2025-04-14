9434ee256279e0d33352440c0b194169c18a9174
rimgnyw <markus.wingmyr@telia.com>
2025-04-14 16:12:56 +0200

    add ability to specifiy output file

--- markup-language/Program.cs
+++ markup-language/Program.cs
@@ -18,25 +18,35 @@ class Program {
     private static bool hadError = false;
     static void Main(string[] args) {
         bool fullDoc = false;
-        string? path = null;
+        bool doOut = false;
+        string? fileOut = null;
+        string? source = null;
         foreach (string arg in args) {
-            // check for -d argument
-            if (arg == "-d" && path == null)
+            if (arg == "-d")
+                // check for -d argument
                 fullDoc = true;
-            // first argument that isn't an option is assumed to be file path
-            else if (path == null && !arg.StartsWith("-")) {
-                path = arg;
+            else if (arg == "-o")
+                doOut = true;
+            else if (source == null && !arg.StartsWith("-")) {
+                // first argument that isn't an option is the source file
+                source = arg;
+            }
+            else if (doOut && source != null && !arg.StartsWith("-") && fileOut == null) {
+                // specify the output file
+                fileOut = arg;
             }
             else {
                 throw new Exception("Invalid arguments");
             }
 
         }
-        if (path == null)
+        if (source == null)
             throw new Exception("Missing file");
+        if (doOut && fileOut == null)
+            throw new Exception("'-o' option given but not output file specified");
 
 
-        runFile(path, fullDoc);
+        runFile(source, fileOut, fullDoc);
         // runPrompt();
         // runFile("../test2.tst");
     }
@@ -57,10 +67,10 @@ class Program {
     static string run(String source) {
         Lexer lexer = new Lexer(source);
         List<Token> tokens = lexer.scanTokens();
-        Console.WriteLine();
-        foreach (Token token in tokens)
-            Console.WriteLine(token.type);
-        Console.WriteLine();
+        // Console.WriteLine();
+        // foreach (Token token in tokens)
+        //     Console.WriteLine(token.type);
+        // Console.WriteLine();
         Parser parser = new Parser(tokens);
         ParseTree result = parser.parse();
 
@@ -72,18 +82,19 @@ class Program {
         return result.process();
     }
 
-    static void runFile(String path, bool fullDoc) {
+    static void runFile(String path, string? output, bool fullDoc) {
+        output ??= "./out.html"; // default value for output if null
         byte[] bytes = File.ReadAllBytes(path);
         String content = Encoding.Default.GetString(bytes);
         // Console.WriteLine(content);
         string processed = run(content);
         if (fullDoc) {
             string finalOutput = HTML_TEMPLATE_START + processed + HTML_TEMPLATE_END;
-            File.WriteAllText("../output.html", finalOutput);
+            File.WriteAllText(output, finalOutput);
         }
         else
-            File.WriteAllText("../output.html", processed);
-        Console.WriteLine("saved to output.html");
+            File.WriteAllText(output, processed);
+        Console.WriteLine($"saved to {output}");
     }
 
     public static ParseError error(int line, string message) {
